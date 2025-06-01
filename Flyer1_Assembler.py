2410c265026755536c489cd5cfc689a81e81e62d
KemalAbizar <73152134+kemalabizar@users.noreply.github.com>
2025-06-01 20:17:55 +0700

    Corrected some errors

--- Flyer1_Assembler.py
+++ Flyer1_Assembler.py
@@ -163,7 +163,7 @@ def hex_obj(machine_code, static_dict, dynamic_dict):
     return compiled
 
 # TOKENIZE Pt.1: Turn source file into string
-srcstr = sourcetostring('NewAssemblyTypesetTest.asm')
+srcstr = sourcetostring(sys.argv[1])
 # TOKENIZE Pt.2: Delete comments in string
 delcom = deletecomments(srcstr)
 # TOKENIZE Pt.3: Tokenize by words
